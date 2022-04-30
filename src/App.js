1ea582f90394185bdbda61c21dff55cf94b9507f
ccnextgen <103283908+ccnextgen@users.noreply.github.com>
2022-05-01 00:14:36 +0530

    nav-bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/nav-bar/NavBar";
 import SignIn from "./components/sign-in/SignIn";
 import SignUp from "./components/sign-up/SignUp";
 import Testing from "./components/testing/testing";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/testing" element={<Testing />} />
-      </Routes>
-    </Router>
+    <>
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/testing" element={<Testing />} />
+        </Routes>
+      </Router>
+    </>
+
+
   );
 }
 
---
+++ src/components/nav-bar/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+function NavBar() {
+  return (
+    <div className="nav-bar-container">
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          About
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
