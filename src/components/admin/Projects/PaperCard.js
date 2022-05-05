fb940662bb6c72c067a2d6947161b656258bdeab
ccnextgen <103283908+ccnextgen@users.noreply.github.com>
2022-05-05 23:07:12 +0530

    paper collapsed

--- src/components/admin/Projects/PaperCard.js
+++ src/components/admin/Projects/PaperCard.js
@@ -1,9 +1,37 @@
-function PaperCard({ paper }) {
-  console.log(`test`, paper)
+import { useState } from 'react';
+import './PaperCard.css';
+function PaperCard(paper) {
+  const [isCollapse, setIsCollapse] = useState(true)
+  console.log(`test`, paper.paper)
 
-  return (<div>
-    <h1>{paper.name}</h1>
-  </div>);
+  return (
+    <div className="class-container">
+      <div className='paper-card' onClick={() => setIsCollapse(!isCollapse)}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                {paper.paper.id}
+              </td>
+              <td>
+                {paper.paper.name}
+              </td>
+              <td>
+                {paper.paper.year}
+              </td>
+              <td>
+                {paper.paper.type}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {
+        isCollapse ? <div></div> : <div>
+          <h1>Collapsed</h1>
+        </div>
+      }
+    </div>);
 }
 
 export default PaperCard;
